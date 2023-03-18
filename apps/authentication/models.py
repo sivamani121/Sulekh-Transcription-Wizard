46d22ac44727f19b1c09af4aa5b087680583716f
siva mani <sivamavi212@gmail.com>
2023-03-18 23:31:55 +0530

    models

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -5,4 +5,45 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.db import models
 
+
+class Annotator(models.Model):
+    aid = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=60)
+    username= models.CharField(max_length=60)
+    score = models.BigIntegerField()
+    logged_in =models.BooleanField(default=False)
+    password = models.TextField(max_length=200)
+    mobileno = models.CharField(max_length=20)
+    email = models.CharField(max_length=70)
+    def __str__(self) -> str:
+        t= str(self.id)+"  "+self.name
+        return t
+
+
+class Verifier(models.Model):
+    vid = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=60)
+    score = models.BigIntegerField()
+    username= models.CharField(max_length=60)
+    logged_in =models.BooleanField(default=False)
+    password = models.TextField(max_length=200)
+    mobileno = models.CharField(max_length=20)
+    email = models.CharField(max_length=70)
+    def __str__(self) -> str:
+        t= str(self.id)+"  "+self.name
+        return t
+    
+
+class Confirmer(models.Model):
+    vid = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=60)
+    username= models.CharField(max_length=60)
+    logged_in =models.BooleanField(default=False)
+    password = models.TextField(max_length=200)
+    mobileno = models.CharField(max_length=20)
+    email = models.CharField(max_length=70)
+    def __str__(self) -> str:
+        t= str(self.id)+"  "+self.name
+        return t
+
 # Create your models here.
--- apps/home/models.py
+++ apps/home/models.py
@@ -5,6 +5,36 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.db import models
 from django.contrib.auth.models import User
+from apps.authentication.models import Annotator,Verifier,Confirmer 
+
+
+
+class Sentence(models.Model):
+    sno = models.BigIntegerField(primary_key=True)
+    noofwords= models.IntegerField()
+    status = models.IntegerField(models.constraints(MaxValueValidator=3,MinValueValidator=0))
+    def __str__(self) -> str:
+        return str(self.sno)
+
+class Words(models.Model):
+    wdno = models.IntegerField()
+    sno = models.ForeignKey(Sentence,on_delete=models.CASCADE)
+    tag = models.CharField(max_length=1)
+    word = models.CharField(max_length=60)
+    ans = models.CharField()
+    def __str__(self) -> str:
+        return str(self.word)
+
+
+class Answered(models.Model):
+    aid = models.ForeignKey(Annotator,on_delete=models.DO_NOTHING)
+    sno = models.ForeignKey(Sentence,on_delete=models.CASCADE)
+class Verified(models.Model):
+    vid = models.ForeignKey(Verifier,on_delete=models.DO_NOTHING)
+    sno = models.ForeignKey(Sentence,on_delete=models.CASCADE)
+class Answered(models.Model):
+    aid = models.ForeignKey(Confirmer,on_delete=models.DO_NOTHING)
+    sno = models.ForeignKey(Sentence,on_delete=models.CASCADE)
 
 # Create your models here.
 
