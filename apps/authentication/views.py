c4e80132a2eecc2f80055ffd23d185b3f35d87f3
abhigeddam <abhigeddam94@gmail.com>
2023-03-21 00:28:25 +0530

    added databases
    added user added Data,added forms,login template

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -10,17 +10,22 @@ from .forms import LoginForm, SignUpForm
 
 
 def login_view(request):
-    
+
+    form = LoginForm(request.POST or None)
 
     msg = None
 
     if request.method == "POST":
-        form = LoginForm(request.POST or None)
 
         if form.is_valid():
-            form.save()
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
             user = authenticate(username=username, password=password)
-            return redirect("/")
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+            else:
+                msg = 'Invalid credentials'
         else:
             msg = 'Error validating the form'
 
